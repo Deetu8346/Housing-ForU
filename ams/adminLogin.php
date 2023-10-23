2e6bff57387bf3bd2338f8e8b1a5b460a3f27522
Aditya Srivastava <97466527+Deetu8346@users.noreply.github.com>
2023-10-24 01:28:38 +0530

    Update adminLogin.php

--- ams/adminLogin.php
+++ ams/adminLogin.php
@@ -138,7 +138,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <a href="mailto:contact@yourwebsite.com"><i class="fas fa-envelope"></i></a>
       <a href="#"><i class="fab fa-linkedin-in"></i></a>
       <a href="526894587"><i class="fa fa-phone"></i></a>
-      <a href="https://github.com/alkatrivedi/Apartment-Management-System"><i class="fab fa-github"></i></a>
+      <a href="https://github.com/Deetu8346/Apartment-Management-System"><i class="fab fa-github"></i></a>
 
     </div>
 
